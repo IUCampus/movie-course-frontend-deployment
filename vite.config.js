8bc4a2023e43d230dd053770b9325e1e93940434
Chigozie Francis <franksoft1@yahoo.com>
2026-07-19 13:10:08 +0200

    Initial Commits

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: process.env.VITE_API_BASE_URL || 'https://movie-course-backend-deployment.onrender.com',
+          target: env.VITE_API_BASE_URL || 'https://movie-course-backend-deployment.onrender.com',
           changeOrigin: true,
         },
       },
